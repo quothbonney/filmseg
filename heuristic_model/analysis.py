873bd524c3a593f4c38295c965a104e4a9f579b9
Jackie Carson <100622290+quothbonney@users.noreply.github.com>
2023-06-20 18:10:33 -0500

    Initial Commit

---
+++ heuristic_model/analysis.py
@@ -0,0 +1,154 @@
+import numpy as np
+import cv2
+from mask import Mask
+# from tkinter import *
+# from tkinter import filedialog
+from matplotlib.figure import Figure
+# from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
+
+
+def save(widths):
+    txt_content = ""
+    for w in widths:
+        point = str(w)
+        txt_content += point + "\n"
+
+    fob=filedialog.asksaveasfile(filetypes=[('text file','*.txt')],
+        defaultextension='.txt',initialdir='D:\\my_data\\my_html',
+        mode='w')
+    try:
+        fob.write(txt_content)
+        fob.close()
+    except :
+        print (" There is an error...")
+
+
+def line_analysis(mask): 
+    mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
+    thresh = cv2.threshold(mask, 0, 255, cv2.THRESH_OTSU + cv2.THRESH_BINARY)[1]
+    
+    widths = []
+    for row in thresh:
+        width = cv2.countNonZero(row)
+        if width > 10:
+            widths.append(width)
+    
+    return widths
+
+
+def show_line_analysis(widths):
+    window = Toplevel()
+    window.title("Line by Line Analysis")
+    window.geometry('%sx%s' % (600, 600))
+    window.configure(background='grey')
+
+    fig = Figure(figsize=(5,5), dpi=100) 
+    plot1 = fig.add_subplot(111) # No I don't know why.
+    plot1.plot(widths)
+    plot1.set_xlabel('Row (px)')
+    plot1.set_ylabel('Highlighted px')
+
+    canvas = FigureCanvasTkAgg(fig, master=window)
+    canvas.draw()
+
+    btn = Button(window, text="Save Data", width=13, command=lambda: save(widths))
+    btn.place(x=20, y=20)
+
+
+    canvas.get_tk_widget().pack()
+
+    toolbar = NavigationToolbar2Tk(canvas, window)
+    toolbar.update()
+
+    canvas.get_tk_widget().pack()
+
+
+
+def mask_size(mask):
+    # Count number of pixels in a mask
+    gray = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
+    thresh = cv2.threshold(gray, 128, 255, cv2.THRESH_OTSU + cv2.THRESH_BINARY)[1]  # Set all pixels = 1, 0;
+    pixels = cv2.countNonZero(thresh)  # Basically just a sum of all pixels
+    return pixels
+
+
+def saturation_histogram(image, channel_num, hsvize=True):
+    if hsvize is True:
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+    channel = image[:,:,channel_num]
+    x = channel.flatten()
+
+    fil = [p for p in x if p > 10]
+    
+    print(f"Channel {channel_num} average: " + str(sum(fil)/len(fil)))
+
+    return fil
+
+
+def show_saturations(sats, ctype):
+
+    window = Toplevel()
+    window.title("Saturation Histogram")
+    window.geometry('%sx%s' % (600, 600))
+    window.configure(background='grey')
+
+    fig = Figure(figsize=(5,5), dpi=100) 
+    plot1 = fig.add_subplot(111) # No I don't know why. 
+  
+
+    counts, bins = np.histogram(sats, density=True, bins=256, range=(0, 255))
+    plot1.hist(bins[:-1], bins, weights=counts)
+    plot1.set_xlabel(ctype)
+    plot1.set_ylabel('Probability')
+   
+    canvas = FigureCanvasTkAgg(fig, master=window)
+    canvas.draw()
+
+    btn = Button(window, text="Save Data", width=13, command=lambda: save(sats))
+    btn.place(x=20, y=20)
+
+
+    canvas.get_tk_widget().pack()
+
+    toolbar = NavigationToolbar2Tk(canvas, window)
+    toolbar.update()
+
+    canvas.get_tk_widget().pack()
+
+
+def errors(type, deposit, is_auto):
+    mask = Mask(type, deposit)
+    sobel = mask.sobel_mask()  # Get sobel mask
+    edges = mask.edge_sobel_mask()
+    if is_auto:
+        final_mask = cv2.threshold(sobel-edges, 0, 255, cv2.THRESH_OTSU + cv2.THRESH_BINARY)[1]
+    else:
+        final_mask = cv2.threshold(sobel, 0, 255, cv2.THRESH_OTSU + cv2.THRESH_BINARY)[1]
+        print("Manual Mask Confirmed")
+    return final_mask
+
+
+def show_errors(mask, image):
+    green = np.zeros(image.shape, np.uint8)
+    green[:] = (57, 255, 20)
+    green_mask = cv2.bitwise_or(green, image, mask=mask)
+    dst = cv2.addWeighted(green_mask, 0.5, image, 0.7, 0)
+
+    return dst
+
+
+def percent_imp(errors_mask, original_mask, image):
+    result = image.copy()
+    white= np.zeros(result.shape, np.uint8)
+    white[:] = (255, 255, 255)
+    errors_masked = cv2.bitwise_and(white, white, mask=errors_mask)
+    dep_masked = cv2.bitwise_and(white, white, mask=original_mask)
+
+    error_size = mask_size(errors_masked)
+    deposit_size = mask_size(dep_masked)
+    ratio = error_size / deposit_size
+
+    ratio_string = "{0:.5f}%".format(ratio * 100)
+
+    return ratio_string
---
+++ heuristic_model/mask.py
@@ -0,0 +1,98 @@
+import cv2
+import numpy as np
+import json
+
+f = {
+    "PbO2": [
+        [8, 50, 20],
+        [60, 255, 250]
+    ],
+    "PEDOT": [
+        [90, 50, 20],
+         [120, 255, 205]
+    ],
+    "PbI2": [
+        [20, 100, 20],
+         [35, 255, 205]
+    ],
+    "Perovskite": [
+        [15, 30, 0],
+        [80, 255, 255]
+    ],
+	"Copper": [
+		[5, 60, 50],
+		[60, 200, 200]
+	],
+	"Carbon": [
+		[0, 10, 0],
+		[255, 130, 80]
+	],
+	"Polyaniline": [
+		[0, 0, 0],
+		[150, 150, 100]
+	],
+    "ZnO": [
+      [0, 0, 0],
+      [100, 100, 255]
+    ]
+
+}
+
+bound_map = f
+
+class Mask:
+    def __init__(self, type_string, dep):
+        self.type = type_string
+        # Expects self.deposition as RGB image
+        self.deposition = dep
+
+    def deposition_mask(self, image):
+        local = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        lower = np.array(bound_map[self.type][0])
+        upper = np.array(bound_map[self.type][1])
+        mask = cv2.inRange(local, lower, upper)
+
+        # inRange() function defines mask
+        self.deposition = cv2.cvtColor(
+            cv2.bitwise_and(image, image, mask=mask),
+            cv2.COLOR_HSV2BGR)
+        return mask
+
+    def sobel_mask(self):
+        img_gray = cv2.cvtColor(np.array(self.deposition), cv2.COLOR_BGR2GRAY)
+        gauss = cv2.GaussianBlur(img_gray, (3, 3), 0)  # Small Gaussian to remove edge noise
+
+        # Sobel solves for x and y separately and then combines them
+        grad_x = cv2.Sobel(gauss, cv2.CV_64F, 1, 0, ksize=5)
+        grad_y = cv2.Sobel(gauss, cv2.CV_64F, 0, 1, ksize=5)
+        abs_grad_x = cv2.convertScaleAbs(grad_x)
+        abs_grad_y = cv2.convertScaleAbs(grad_y)
+
+        # Merges sobel together and then thresholds to allow bit counting
+        grad = cv2.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0)
+        mask = cv2.threshold(grad, 122, 255, 0)[1]
+
+        return mask
+
+    def edge_sobel_mask(self):
+        # Basically the same as sobel_mask()
+        # Erodes img to just the edges of the deposit mask
+        kernel = np.ones((5, 5), np.uint8)
+        img_gray = cv2.cvtColor(np.array(self.deposition), cv2.COLOR_BGR2GRAY)
+        gauss = cv2.GaussianBlur(img_gray, (3, 3), 0)
+        erosion = cv2.erode(gauss, kernel, iterations=1)
+        dilate = cv2.dilate(erosion, kernel, iterations=1)
+
+        # Same process as in sobel()
+
+        grad_x = cv2.Sobel(dilate, cv2.CV_64F, 1, 0, ksize=5)
+        grad_y = cv2.Sobel(dilate, cv2.CV_64F, 0, 1, ksize=5)
+
+        abs_grad_x = cv2.convertScaleAbs(grad_x)
+        abs_grad_y = cv2.convertScaleAbs(grad_y)
+
+        grad = cv2.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0)
+
+        mask = cv2.threshold(grad, 120, 255, 0)[1]
+
+        return mask
